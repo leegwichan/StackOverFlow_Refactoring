90188b1b290967f73765be90ab85c137cc50818e
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-02-07 20:22:13 +0900

    SRC-7 refactor(VoteMapper) : 투표 관련 Mapper 들을 Mapstruct의 @Mapper 를 이용하여 사용
    - Mapstruct의 레퍼런스를 참고하여 리팩토링 실시
    - 테스트 코드 정상 작동 확인

--- src/main/java/com/team17/preProject/domain/vote/mapper/VoteAnswerMapper.java
+++ src/main/java/com/team17/preProject/domain/vote/mapper/VoteAnswerMapper.java
@@ -3,14 +3,12 @@ package com.team17.preProject.domain.vote.mapper;
 import com.team17.preProject.domain.vote.dto.VoteAnswerDto;
 import com.team17.preProject.domain.vote.entity.VoteAnswer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface VoteAnswerMapper {
-    default VoteAnswerDto.Response voteAnswerToVoteAnswerResponseDto(VoteAnswer voteAnswer){
-        return new VoteAnswerDto.Response(
-                voteAnswer.getMember().getMemberId(),
-                voteAnswer.getAnswer().getAnswerId(),
-                voteAnswer.getVote()
-        );
-    }
+
+    @Mapping(target = "memberId", expression = "java(voteAnswer.getMember().getMemberId())")
+    @Mapping(target = "answerId", expression = "java(voteAnswer.getAnswer().getAnswerId())")
+    VoteAnswerDto.Response voteAnswerToVoteAnswerResponseDto(VoteAnswer voteAnswer);
 }
--- src/main/java/com/team17/preProject/domain/vote/mapper/VoteQuestionMapper.java
+++ src/main/java/com/team17/preProject/domain/vote/mapper/VoteQuestionMapper.java
@@ -3,14 +3,12 @@ package com.team17.preProject.domain.vote.mapper;
 import com.team17.preProject.domain.vote.dto.VoteQuestionDto;
 import com.team17.preProject.domain.vote.entity.VoteQuestion;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface VoteQuestionMapper {
-    default VoteQuestionDto.Response voteQuestionToVoteQuestionResponseDto(VoteQuestion voteQuestion){
-        return new VoteQuestionDto.Response(
-                voteQuestion.getMember().getMemberId(),
-                voteQuestion.getQuestion().getQuestionId(),
-                voteQuestion.getVote()
-        );
-    }
+
+    @Mapping(target = "memberId", expression = "java(voteQuestion.getMember().getMemberId())")
+    @Mapping(target = "questionId", expression = "java(voteQuestion.getQuestion().getQuestionId())")
+    VoteQuestionDto.Response voteQuestionToVoteQuestionResponseDto(VoteQuestion voteQuestion);
 }
