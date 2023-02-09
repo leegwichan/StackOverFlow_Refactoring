21030c6c1d867b1e94ea83ab693eb75dc42ad980
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-02-09 21:39:46 +0900

    SCR-7 refactor(mapstruct) : Mapstruct 레퍼런스를 참고하여 리팩토링 실시
    - 우선적으로 AnswerMapper, QuestionMapper 리팩토링 실시
    - default 를 이용해 수동으로 mapping한 부분을 Annotation을 이용해 Mapstruct 에서 하도록 변경함
    - 다른 Mapper에 있는 method도 인식해서 사용할 수 있도록 @Mapper.uses에 class 추가

--- src/main/java/com/team17/preProject/domain/answer/mapper/AnswerMapper.java
+++ src/main/java/com/team17/preProject/domain/answer/mapper/AnswerMapper.java
@@ -2,80 +2,24 @@ package com.team17.preProject.domain.answer.mapper;
 
 import com.team17.preProject.domain.answer.dto.AnswerDto;
 import com.team17.preProject.domain.answer.entity.Answer;
-import com.team17.preProject.domain.member.dto.MemberDto;
-import com.team17.preProject.domain.member.entity.Member;
-import com.team17.preProject.domain.question.dto.QuestionDto;
-import com.team17.preProject.domain.question.entity.Question;
+import com.team17.preProject.domain.member.mapper.MemberMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {MemberMapper.class})
 public interface AnswerMapper {
-    default AnswerDto.Response answerToAnswerResponseDto(Answer answer){
-      return new AnswerDto.Response(
-              answer.getAnswerId(),
-              answer.getContent(),
-              answer.getVote(),
-              answer.getCreatedAt(),
-              answer.getModifiedAt(),
-              memberToMemberSubResponseDto(answer.getMember())
-      );
-    }
 
-    default AnswerDto.FullResponse answerToAnswerFullResponseDto(Answer answer){
-        return new AnswerDto.FullResponse(
-                answer.getAnswerId(),
-                answer.getContent(),
-                answer.getVote(),
-                answer.getCreatedAt(),
-                answer.getModifiedAt(),
-                questionToQuestionSubResponseDto(answer.getQuestion())
-        );
-    }
+    @Mapping(target = "question.questionId", source = "questionId")
+    @Mapping(target = "member.memberId", source = "memberId")
+    Answer answerPostDtoToAnswer(AnswerDto.Post post);
 
-    default List<AnswerDto.FullResponse> answersToAnswerFullResponseDtos(List<Answer> answers){
-        return answers.stream()
-                .map(answer -> answerToAnswerFullResponseDto(answer))
-                .collect(Collectors.toList());
-    }
-    default List<AnswerDto.Response> answersToAnswerResponseDtos(List<Answer> answers){
-        return answers.stream()
-                .map(answer -> answerToAnswerResponseDto(answer))
-                .collect(Collectors.toList());
-    }
-    default Answer answerPostDtoToAnswer(AnswerDto.Post post){
-        Answer answer = new Answer();
-        answer.setContent(post.getContent());
-
-        Question question = new Question();
-        question.setQuestionId(post.getQuestionId());
-        answer.setQuestion(question);
-
-        Member member = new Member();
-        member.setMemberId(post.getMemberId());
-        answer.setMember(member);
-
-        return answer;
-    }
     Answer answerPatchDtoToAnswer(AnswerDto.Patch patch);
-    MemberDto.SubResponse memberToMemberSubResponseDto(Member member);
-    default QuestionDto.SubResponse questionToQuestionSubResponseDto(Question question){
 
-        Answer bestAnswer = question.getBestAnswer();
-
-        return new QuestionDto.SubResponse(
-                question.getQuestionId(),
-                question.getTitle(),
-                question.getContent(),
-                question.getView(),
-                question.getVote(),
-                question.getCreatedAt(),
-                question.getModifiedAt(),
-                memberToMemberSubResponseDto(question.getMember()),
-                question.getAnswers().size(),
-                bestAnswer != null ? bestAnswer.getAnswerId() : null
-        );
-    }
+    AnswerDto.Response answerToAnswerResponseDto(Answer answer);
+    List<AnswerDto.Response> answersToAnswerResponseDtos(List<Answer> answers);
+    AnswerDto.FullResponse answerToAnswerFullResponseDto(Answer answer);
+    List<AnswerDto.FullResponse> answersToAnswerFullResponseDtos(List<Answer> answers);
 }
