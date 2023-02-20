c50b58ef186a1bb26402770b55e738e837907bc7
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-02-20 19:31:09 +0900

    SCR-22 test(MemberService) : findMemberTest() 작성
    - findMemberTest 작성
    - 일반적인 경우, 해당 Member가 없어서 예외가 발생하는 경우 Unit Test 작성
    - 테스트 통과 확인 완료

---
+++ src/test/java/com/team17/preProject/domain/member/service/MemberServiceTest.java
@@ -0,0 +1,47 @@
+package com.team17.preProject.domain.member.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+
+import com.team17.preProject.domain.member.entity.Member;
+import com.team17.preProject.domain.member.repository.MemberRepository;
+import com.team17.preProject.exception.businessLogic.BusinessLogicException;
+import com.team17.preProject.helper.email.EmailSender;
+import com.team17.preProject.helper.password.NewPassword;
+import com.team17.preProject.helper.stub.MemberStub;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import java.util.Optional;
+
+@SpringBootTest(classes = {MemberServiceImpl.class})
+public class MemberServiceTest {
+
+    @Autowired private MemberService memberService;
+    @MockBean private MemberRepository repository;
+    @MockBean private EmailSender emailSender;
+    @MockBean private NewPassword newPassword;
+
+    private static final Member MEMBER_STUB = MemberStub.ENTITY;
+
+    @Test
+    void findMemberTest() {
+        Member expected = MEMBER_STUB;
+        given(repository.findById(1L)).willReturn(Optional.of(expected));
+
+        Member result = memberService.findMember(1L);
+
+        assertThat(result).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    @Test
+    void findMemberTest_whenMemberNotFound() {
+        given(repository.findById(1L)).willReturn(Optional.empty());
+
+        Exception result = assertThrows(BusinessLogicException.class,
+                () -> memberService.findMember(1L));
+        assertThat(result.getMessage()).isEqualTo("Member not found");
+    }
+}
