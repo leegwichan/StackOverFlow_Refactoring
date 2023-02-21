82ab7d95979f06d6adeb9f97d572f467b5ed4093
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-02-21 14:12:28 +0900

    SCR-31 refactor(TemporaryPassword) : 클래스 변수 지정
    - 임시 비밀번호에 들어가는 문자(시작, 끝), 비밀번호 길이 등의 하드코딩한 부분을 상수화하여 사용
    - Random 도 매 실행마다 객체를 만드는 대신 한번 만든 객체를 여러번 사용하는 것으로 수정

--- src/main/java/com/team17/preProject/helper/password/TemporaryPassword.java
+++ src/main/java/com/team17/preProject/helper/password/TemporaryPassword.java
@@ -7,27 +7,31 @@ import org.springframework.stereotype.Component;
 import java.util.Random;
 
 @Component
-@RequiredArgsConstructor
 public class TemporaryPassword {
 
+    private static final char START_LETTER = 'a';
+    private static final char END_LETTER = 'z';
+    private static final int PASSWORD_LENGTH = 10;
+
     private final BCryptPasswordEncoder encoder;
+    private final Random random = new Random();
+
+    public TemporaryPassword(BCryptPasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
 
     public String[] create(){
 
-        int leftLimit = 97; // letter 'a'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 10;
-        Random random = new Random();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int)
-                    (random.nextFloat() * (rightLimit - leftLimit + 1));
-            sb.append((char) randomLimitedInt);
+        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+            int randomLetter =
+                    START_LETTER + (int) (random.nextFloat() * (START_LETTER - END_LETTER + 1));
+            sb.append((char) randomLetter);
         }
 
-        String generatedString = sb.toString();
-        String encodeString = encoder.encode(generatedString);
+        String generatedPassword = sb.toString();
+        String encodedPassword = encoder.encode(generatedPassword);
 
-        return new String[]{generatedString, encodeString};
+        return new String[]{generatedPassword, encodedPassword};
     }
 }
