eb14b268d91142498afc3d411c5205517644be82
leegwichan <cnddkscndgus@naver.com>
2023-02-24 17:56:15 +0900

    SCR-32 refactor(S3Upload) : 파일 확장자 일치 확인 부분 리팩토링 실시
    - 허용하는 파일 확장자가 하드 코딩 되어있어 class 상수 취급함
    - 해당 부분이 내용이 길어져 메서드 분리 실시

--- src/main/java/com/team17/preProject/helper/upload/S3UploadImpl.java
+++ src/main/java/com/team17/preProject/helper/upload/S3UploadImpl.java
@@ -1,6 +1,5 @@
 package com.team17.preProject.helper.upload;
 
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.team17.preProject.exception.businessLogic.BusinessLogicException;
@@ -8,7 +7,6 @@ import com.team17.preProject.exception.businessLogic.ExceptionCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -17,22 +15,18 @@ import java.time.format.DateTimeFormatter;
 @Service
 public class S3UploadImpl implements S3Upload{
 
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
+    private static final String FILE_EXTENSION_DELIMITER = ".";
+    private static final String[] ALLOWED_FILE_EXTENSION = new String[]{".jpg", ".png"};
 
-    @Value("${cloud.aws.s3.dir}")
-    private String dir;
-
-    // private String[] imageFileExtension = new String[]{".jpg", ".png"};
-
+    @Value("${cloud.aws.s3.bucket}") private String bucket;
+    @Value("${cloud.aws.s3.dir}") private String dir;
     private final AmazonS3Client s3Client;
 
 
     @Override
     public String upload(InputStream inputStream, String originFileName, String fileSize) {
 
-        if(!originFileName.substring(originFileName.length()-4).equals(".jpg") &&
-                !originFileName.substring(originFileName.length()-4).equals(".png")){
+        if(!isAllowedExtension(originFileName)){
             throw new BusinessLogicException(ExceptionCode.NOT_IMAGE_EXTENSION);
         }
 
@@ -45,5 +39,18 @@ public class S3UploadImpl implements S3Upload{
         s3Client.putObject(bucket, s3FileName, inputStream, objMeta);
 
         return s3Client.getUrl(bucket, s3FileName).toString();
+    }
+
+    private boolean isAllowedExtension(String fileName) {
+        for (String extension : ALLOWED_FILE_EXTENSION) {
+            if (isMatchedExtension(fileName, extension)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isMatchedExtension(String filename, String extension) {
+        return filename.substring(filename.lastIndexOf(FILE_EXTENSION_DELIMITER)).equals(extension);
     }
 }
