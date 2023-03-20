621999198c761ba1b53d003fae42443c5afc87b0
leegwichan <cnddkscndgus@naver.com>
2023-03-20 19:04:46 +0900

    SCR-25 test(FollowQuestionService) : create() 테스트 코드 작성
    - 일반적인 경우, 이미 follow 한 경우 테스트 코드 작성
    - 테스트 통과 확인

--- src/test/java/com/team17/preProject/domain/follow/service/FollowQuestionServiceTest.java
+++ src/test/java/com/team17/preProject/domain/follow/service/FollowQuestionServiceTest.java
@@ -1,6 +1,7 @@
 package com.team17.preProject.domain.follow.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -9,8 +10,10 @@ import com.team17.preProject.domain.follow.entity.FollowQuestion;
 import com.team17.preProject.domain.follow.repository.FollowQuestionRepository;
 import com.team17.preProject.domain.member.service.MemberService;
 import com.team17.preProject.domain.question.service.QuestionService;
+import com.team17.preProject.exception.businessLogic.BusinessLogicException;
 import com.team17.preProject.helper.stub.FollowQuestionStub;
 import com.team17.preProject.helper.stub.MemberStub;
+import com.team17.preProject.helper.stub.QuestionStub;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +21,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest(classes = {FollowQuestionServiceImpl.class})
 public class FollowQuestionServiceTest {
@@ -37,5 +41,30 @@ public class FollowQuestionServiceTest {
                 followQuestionService.findFollowQuestionsByMember(1,1,MemberStub.ENTITY.getMemberId());
 
         assertThat(result.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    void createFollowQuestionTest() {
+        given(memberService.findMember(anyLong())).willReturn(MemberStub.getChangeableEntity());
+        given(questionService.findQuestion(anyLong())).willReturn(QuestionStub.getChangeableEntity());
+        given(repository.findByMemberAndQuestion(any(), any())).willReturn(Optional.empty());
+        given(repository.save(any())).willReturn(FollowQuestionStub.getEntity(3L));
+
+        FollowQuestion result = followQuestionService.createFollowQuestion(3L, 3L);
+
+        assertThat(result).usingRecursiveComparison().isEqualTo(FollowQuestionStub.getEntity(3L));
+    }
+
+    @Test
+    void createFollowQuestionTest_whenAlreadyExist() {
+        given(memberService.findMember(anyLong())).willReturn(MemberStub.getChangeableEntity());
+        given(questionService.findQuestion(anyLong())).willReturn(QuestionStub.getChangeableEntity());
+        given(repository.findByMemberAndQuestion(any(), any()))
+                .willReturn(Optional.of(FollowQuestionStub.getEntity(3L)));
+
+        Exception result = assertThrows(BusinessLogicException.class,
+                () -> followQuestionService.createFollowQuestion(3L, 3L));
+
+        assertThat(result.getMessage()).isEqualTo("Already follow post");
     }
 }
