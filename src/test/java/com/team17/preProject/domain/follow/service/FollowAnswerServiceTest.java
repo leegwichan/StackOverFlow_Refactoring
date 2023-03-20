cd84316a076d10860122b7a0f4e5e49e69001558
leegwichan <cnddkscndgus@naver.com>
2023-03-20 19:49:42 +0900

    SCR-25 test(FollowAnswer) : create() 테스트 코드 작성
    - 정상적인 경우, 이미 존재하는 경우 테스트 코드 작성
    - 테스트 통과 확인 완료

--- src/test/java/com/team17/preProject/domain/follow/service/FollowAnswerServiceTest.java
+++ src/test/java/com/team17/preProject/domain/follow/service/FollowAnswerServiceTest.java
@@ -1,6 +1,7 @@
 package com.team17.preProject.domain.follow.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -9,6 +10,8 @@ import com.team17.preProject.domain.answer.service.AnswerService;
 import com.team17.preProject.domain.follow.entity.FollowAnswer;
 import com.team17.preProject.domain.follow.repository.FollowAnswerRepository;
 import com.team17.preProject.domain.member.service.MemberService;
+import com.team17.preProject.exception.businessLogic.BusinessLogicException;
+import com.team17.preProject.helper.stub.AnswerStub;
 import com.team17.preProject.helper.stub.FollowAnswerStub;
 import com.team17.preProject.helper.stub.MemberStub;
 import org.junit.jupiter.api.Test;
@@ -18,6 +21,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest(classes = {FollowAnswerServiceImpl.class})
 class FollowAnswerServiceTest {
@@ -36,6 +40,31 @@ class FollowAnswerServiceTest {
         Page<FollowAnswer> result = followAnswerService.findFollowAnswerByMember(1,2,3L);
 
         assertThat(result.getContent()).usingRecursiveComparison().isEqualTo(content);
+    }
+
+    @Test
+    void createFollowAnswerTest() {
+        given(memberService.findMember(anyLong())).willReturn(MemberStub.getChangeableEntity());
+        given(answerService.findAnswer(anyLong())).willReturn(AnswerStub.getEntity());
+        given(repository.findByMemberAndAnswer(any(), any())).willReturn(Optional.empty());
+        given(repository.save(any())).willReturn(FollowAnswerStub.getEntity(3L));
+
+        FollowAnswer result = followAnswerService.createFollowAnswer(3L, 3L);
+
+        assertThat(result).usingRecursiveComparison().isEqualTo(FollowAnswerStub.getEntity(3L));
+    }
+
+    @Test
+    void createFollowAnswerTest_whenAlreadyExist() {
+        given(memberService.findMember(anyLong())).willReturn(MemberStub.getChangeableEntity());
+        given(answerService.findAnswer(anyLong())).willReturn(AnswerStub.getEntity());
+        given(repository.findByMemberAndAnswer(any(), any()))
+                .willReturn(Optional.of(FollowAnswerStub.getEntity(3L)));
+
+        Exception result = assertThrows(BusinessLogicException.class,
+                () -> followAnswerService.createFollowAnswer(3L, 3L));
+
+        assertThat(result.getMessage()).isEqualTo("Already follow post");
     }
 
 }
